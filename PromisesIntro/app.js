88f31c12c0b6376dbcf9308d6c88ca145b6adc01
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2023-03-03 22:34:03 -0500

    Completed the callback practice

--- PromisesIntro/app.js
+++ PromisesIntro/app.js
@@ -23,6 +23,33 @@ const fakeRequestPromise = (url) => {
     })
 }
 
+//The code above is a fake request
+
+fakeRequestCallback('books.com/page1', 
+    function (response) {
+    console.log("IT WORKED!!!")
+    console.log(response)
+    fakeRequestCallback('book.com/page2',
+        function(response) {
+            console.log("IT WORKED AGAIN!!!")
+            console.log(response)
+            fakeRequestCallback('book.com/page3',
+            function (response) {
+                console.log("IT WORKED AGAIN (3rd req)!!!")
+                console.log(response)
+            },
+            function (err) {
+                console.log("ERROR (3rd req)!!!", err)
+            })
+        },
+        function (err) {
+            console.log("ERROR (2nd req)!!!", err)
+        })
+}, function (err) {
+    console.log("ERROR!!!", err)
+})
+
+
 
 // fakeRequestCallback('books.com/page1',
 //     function (response) {
@@ -79,25 +106,25 @@ const fakeRequestPromise = (url) => {
 
 // THE CLEANEST OPTION WITH THEN/CATCH
 // RETURN A PROMISE FROM .THEN() CALLBACK SO WE CAN CHAIN!
-fakeRequestPromise('yelp.com/api/coffee/page1')
-    .then((data) => {
-        console.log("IT WORKED!!!!!! (page1)")
-        console.log(data)
-        return fakeRequestPromise('yelp.com/api/coffee/page2')
-    })
-    .then((data) => {
-        console.log("IT WORKED!!!!!! (page2)")
-        console.log(data)
-        return fakeRequestPromise('yelp.com/api/coffee/page3')
-    })
-    .then((data) => {
-        console.log("IT WORKED!!!!!! (page3)")
-        console.log(data)
-    })
-    .catch((err) => {
-        console.log("OH NO, A REQUEST FAILED!!!")
-        console.log(err)
-    })
+// fakeRequestPromise('yelp.com/api/coffee/page1')
+//     .then((data) => {
+//         console.log("IT WORKED!!!!!! (page1)")
+//         console.log(data)
+//         return fakeRequestPromise('yelp.com/api/coffee/page2')
+//     })
+//     .then((data) => {
+//         console.log("IT WORKED!!!!!! (page2)")
+//         console.log(data)
+//         return fakeRequestPromise('yelp.com/api/coffee/page3')
+//     })
+//     .then((data) => {
+//         console.log("IT WORKED!!!!!! (page3)")
+//         console.log(data)
+//     })
+//     .catch((err) => {
+//         console.log("OH NO, A REQUEST FAILED!!!")
+//         console.log(err)
+//     })
 
 
 
